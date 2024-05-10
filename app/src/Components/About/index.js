184ff845fcc6bf8e2e0bca1fe4fad805b02a63a5
Becki Wordsworth <63713673+BeckiWordsworth@users.noreply.github.com>
2024-05-10 23:09:18 +0200

    Updates to timeline

--- app/src/Components/About/index.js
+++ app/src/Components/About/index.js
@@ -19,29 +19,55 @@ class About extends React.Component {
           <hr />
 
           <ul class="timeline">
+            <li>
+              <div class="direction-l">
+                <div class="flag-wrapper">
+                  <span class="hexa" />
+                  <span class="flag">Web Engineering Manager</span>
+                  <span class="time-wrapper">
+                    <span class="time">March 2023 - Present</span>
+                  </span>
+                </div>
+                <div class="desc">
+                  <strong>Flightradar24 AB, Stockholm</strong>
+                  <br /> <br />
+                  Flightradar24 tracks positions of aircraft in real-time and turns that data into engaging consumer and
+                  business applications.
+                  <br /> <br />
+                  Reporting to the CTO, my role includes mentoring and managing multiple cross functional teams working
+                  on a number of different web products. Building web strategy, technical and architectural leadership.
+                  <br /> <br />
+                  Highlights: Javascript/Typescript, Vue, PHP, Laravel, Symfony, MariaDB, Redis, Docker, Kubernetes
+                </div>
+              </div>
+            </li>
 
-          <li>
+            <li>
               <div class="direction-r">
                 <div class="flag-wrapper">
                   <span class="hexa" />
                   <span class="flag">CTO</span>
                   <span class="time-wrapper">
-                    <span class="time">Aug 2023 - Present</span>
+                    <span class="time">Aug 2023 - March 2024</span>
                   </span>
                 </div>
                 <div class="desc">
                   <strong>Hiotlabs AB, Stockholm</strong>
                   <br /> <br />
-                  VivoLogica offers the first cloud based medical record system for pre-clinical in vivo studies on the market, a best-in-class multi-platform compliant with 3R and research regulations making it easy to stay compliant while saving loads of time. 
-                  <br /> <br />Leading all Engineering and Product development in the company, balancing technical and buisness needs to build a product that our customers find intuitive and love to use. Everything from hands on development and growing and leading a team to building strategy.
+                  VivoLogica offers the first cloud based medical record system for pre-clinical in vivo studies on the
+                  market, a best-in-class multi-platform compliant with 3R and research regulations making it easy to
+                  stay compliant while saving loads of time.
                   <br /> <br />
-                  Highlights: Javascript/Typescript, React, React Native, Redux,
-                  Node, MongoDB, Docker, Kubernetes
+                  Leading all Engineering and Product development in the company, balancing technical and buisness needs
+                  to build a product that our customers find intuitive and love to use. Everything from hands on
+                  development and growing and leading a team to building strategy.
+                  <br /> <br />
+                  Highlights: Javascript/Typescript, React, React Native, Redux, Node, MongoDB, Docker, Kubernetes
                 </div>
               </div>
             </li>
 
-          <li>
+            <li>
               <div class="direction-l">
                 <div class="flag-wrapper">
                   <span class="hexa" />
@@ -53,13 +79,14 @@ class About extends React.Component {
                 <div class="desc">
                   <strong>Hiotlabs AB, Stockholm</strong>
                   <br /> <br />
-                  At Hiotlabs we are developing tech to prevent and minimise
-                  property damage.
+                  At Hiotlabs we are developing tech to prevent and minimise property damage.
                   <br /> <br />
-                  My role focusses on developing and leading the strategic technical direction and product strategy for the company alongside direct management of a cross functional team (6 developers, 3 consultants, 1 UX/UI, Product Manager).
+                  My role focusses on developing and leading the strategic technical direction and product strategy for
+                  the company alongside direct management of a cross functional team (6 developers, 3 consultants, 1
+                  UX/UI, Product Manager).
                   <br /> <br />
-                  Highlights: Javascript/Typescript, React, React Native, Redux,
-                  Node, MongoDB, TimescaleDb, Redis, Docker, Kubernetes
+                  Highlights: Javascript/Typescript, React, React Native, Redux, Node, MongoDB, TimescaleDb, Redis,
+                  Docker, Kubernetes
                 </div>
               </div>
             </li>
@@ -76,16 +103,12 @@ class About extends React.Component {
                 <div class="desc">
                   <strong>Hiotlabs AB, Stockholm</strong>
                   <br /> <br />
-                  At Hiotlabs we are developing tech to prevent and minimise
-                  property damage.
-                  <br /> <br />I lead all activities within development at
-                  Hiotlabs including management of people and development
-                  processes while also working hands-on across the whole of our
-                  stack, from frontend/apps all the way to our backend and
-                  hardware, but with a bit more focus on the backend.
+                  At Hiotlabs we are developing tech to prevent and minimise property damage.
+                  <br /> <br />I lead all activities within development at Hiotlabs including management of people and
+                  development processes while also working hands-on across the whole of our stack, from frontend/apps
+                  all the way to our backend and hardware, but with a bit more focus on the backend.
                   <br /> <br />
-                  Highlights: Javascript (ES6+), React, React Native, Redux,
-                  Node, MongoDB, Docker, Kubernetes
+                  Highlights: Javascript (ES6+), React, React Native, Redux, Node, MongoDB, Docker, Kubernetes
                 </div>
               </div>
             </li>
@@ -102,16 +125,13 @@ class About extends React.Component {
                 <div class="desc">
                   <strong>Hiotlabs AB, Stockholm</strong>
                   <br /> <br />
-                  At Hiotlabs we are developing tech to prevent and minimise
-                  property damage.
+                  At Hiotlabs we are developing tech to prevent and minimise property damage.
                   <br /> <br />
-                  My role included working across the whole of our stack, from
-                  our two hybrid apps, frontend, all the way to our backend and
-                  hardware, but with a bit more focus on the backend. I also
-                  worked with our infrastructure.
+                  My role included working across the whole of our stack, from our two hybrid apps, frontend, all the
+                  way to our backend and hardware, but with a bit more focus on the backend. I also worked with our
+                  infrastructure.
                   <br /> <br />
-                  Highlights: Javascript (ES6+), React, React Native, Redux,
-                  Node, MongoDB, Docker, Kubernetes
+                  Highlights: Javascript (ES6+), React, React Native, Redux, Node, MongoDB, Docker, Kubernetes
                 </div>
               </div>
             </li>
@@ -128,12 +148,10 @@ class About extends React.Component {
                 <div class="desc">
                   <strong>Baffin Bay Networks AB, Stockholm</strong>
                   <br /> <br />
-                  Baffin Bay Networks offer a cloud-based Threat Protection
-                  Platform. <br /> <br />
-                  My role is the development of our interactive portal where
-                  customers can customise, monitor, and visualise their threat
-                  protection in real-time. <br /> <br /> Tech: Javascript
-                  (ES6+), React, Redux, Node, MariaDB, Docker
+                  Baffin Bay Networks offer a cloud-based Threat Protection Platform. <br /> <br />
+                  My role is the development of our interactive portal where customers can customise, monitor, and
+                  visualise their threat protection in real-time. <br /> <br /> Tech: Javascript (ES6+), React, Redux,
+                  Node, MariaDB, Docker
                 </div>
               </div>
             </li>
@@ -150,11 +168,9 @@ class About extends React.Component {
                 <div class="desc">
                   <strong>Doctrin AB, Stockholm</strong>
                   <br /> <br />
-                  Javascript development on an existing product with Javascript,
-                  React, Redux, and Node.
+                  Javascript development on an existing product with Javascript, React, Redux, and Node.
                   <br /> <br />
-                  Startup was struggling with money and had to lose 1/4 of their
-                  staff.
+                  Startup was struggling with money and had to lose 1/4 of their staff.
                 </div>
               </div>
             </li>
@@ -171,12 +187,10 @@ class About extends React.Component {
                 <div class="desc">
                   <strong>Technigo AB, Stockholm</strong>
                   <br /> <br />
-                  An intense 12 week program, which is focussed on JavaScript
-                  (ES6), React, and server-side programming with Node,
-                  Express.js and MongoDB. I was coding every day, working on
-                  individual and group assignments which saw us working and
-                  producing projects with clients such as Bonnier, Svenska Spel
-                  and Volunmental in an agile environment.
+                  An intense 12 week program, which is focussed on JavaScript (ES6), React, and server-side programming
+                  with Node, Express.js and MongoDB. I was coding every day, working on individual and group assignments
+                  which saw us working and producing projects with clients such as Bonnier, Svenska Spel and Volunmental
+                  in an agile environment.
                 </div>
               </div>
             </li>
@@ -193,18 +207,13 @@ class About extends React.Component {
                 <div class="desc">
                   <strong>C/MIND AB, Stockholm</strong>
                   <br /> <br />
-                  At C/MIND I specialise in building teams for both large and
-                  small tech companies in Sweden. We are recruiting the best
-                  developers from around the world and helping build and scale
-                  teams for companies like Truecaller, and Apple. • Leading full
-                  cycle recruitment activities within both internally and
-                  externally at C/MIND. • End-end recruitment of software
-                  developers from around the world. • Building relationships
-                  with our clients and developers who have a passion for
-                  technology. • Researching the latest industry standards and
-                  new technologies. • Supporting the relocation and integration
-                  of candidates. • Spearheading our internship scheme. • A
-                  little bit of coding when I have the time......
+                  At C/MIND I specialise in building teams for both large and small tech companies in Sweden. We are
+                  recruiting the best developers from around the world and helping build and scale teams for companies
+                  like Truecaller, and Apple. • Leading full cycle recruitment activities within both internally and
+                  externally at C/MIND. • End-end recruitment of software developers from around the world. • Building
+                  relationships with our clients and developers who have a passion for technology. • Researching the
+                  latest industry standards and new technologies. • Supporting the relocation and integration of
+                  candidates. • Spearheading our internship scheme. • A little bit of coding when I have the time......
                 </div>
               </div>
             </li>
@@ -220,16 +229,12 @@ class About extends React.Component {
                 </div>
                 <div class="desc">
                   <strong>University of Bristol, UK</strong>
-                  <br /> <br />• Fixed-term role on two studies investigating
-                  osteoporosis and scoliosis. • Recruited participants after
-                  identification of eligible people. • Used an SQL-like query
-                  language, along with SPSS and Stata, to analyse a subset of
-                  over 400 million data points and produce charts and reports. •
-                  Made use of regression modelling, significance testing and
-                  data visualisation techniques to interpret and present data at
-                  conferences. • Managed the overall progress of the project by
-                  developing and maintaining study documents and governance
-                  paperwork.
+                  <br /> <br />• Fixed-term role on two studies investigating osteoporosis and scoliosis. • Recruited
+                  participants after identification of eligible people. • Used an SQL-like query language, along with
+                  SPSS and Stata, to analyse a subset of over 400 million data points and produce charts and reports. •
+                  Made use of regression modelling, significance testing and data visualisation techniques to interpret
+                  and present data at conferences. • Managed the overall progress of the project by developing and
+                  maintaining study documents and governance paperwork.
                 </div>
               </div>
             </li>
@@ -258,13 +263,11 @@ class About extends React.Component {
                 </div>
                 <div class="desc">
                   <strong>Sparx, UK</strong>
-                  <br /> <br />• Sparx is an education based start-up,
-                  developing a tablet-based learning system • Fixed-term role on
-                  an iPad-based learning app in an Agile environment. •
-                  Developed course materials and in-app activities for students.
-                  • Analysed user data, using a variety of techniques, to
-                  understand problems and produce better content. • Provided bug
-                  reports and feature ideas to developers using JIRA.
+                  <br /> <br />• Sparx is an education based start-up, developing a tablet-based learning system •
+                  Fixed-term role on an iPad-based learning app in an Agile environment. • Developed course materials
+                  and in-app activities for students. • Analysed user data, using a variety of techniques, to understand
+                  problems and produce better content. • Provided bug reports and feature ideas to developers using
+                  JIRA.
                 </div>
               </div>
             </li>
@@ -279,19 +282,14 @@ class About extends React.Component {
                   </span>
                 </div>
                 <div class="desc">
-                  <strong>
-                    Peninsula Medical School, University of Exeter, UK
-                  </strong>
-                  <br /> <br />• Developed an electronic model and associated
-                  software (LabVIEW) to research the effects of type-2 diabetes
-                  on the microvascular system. • Collected and analysed data
-                  sets consisting of 100,000+ data points using MATLAB, R, SPSS
-                  and Excel to successfully validate my hypothesis. • Developed
-                  and evaluated statistical data for individual studies. •
-                  Extensively used standard parametric and non-parametric tests,
-                  alongside significance testing and other modelling techniques.
-                  • Designed and presented papers at conferences. • Heavy use of
-                  problem-solving skills while developing hardware/software.
+                  <strong>Peninsula Medical School, University of Exeter, UK</strong>
+                  <br /> <br />• Developed an electronic model and associated software (LabVIEW) to research the effects
+                  of type-2 diabetes on the microvascular system. • Collected and analysed data sets consisting of
+                  100,000+ data points using MATLAB, R, SPSS and Excel to successfully validate my hypothesis. •
+                  Developed and evaluated statistical data for individual studies. • Extensively used standard
+                  parametric and non-parametric tests, alongside significance testing and other modelling techniques. •
+                  Designed and presented papers at conferences. • Heavy use of problem-solving skills while developing
+                  hardware/software.
                 </div>
               </div>
             </li>
