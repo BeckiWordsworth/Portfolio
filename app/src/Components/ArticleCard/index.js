701d69d407fef5e75077cbdcbf35958071444eeb
Becki Wordsworth <63713673+BeckiWordsworth@users.noreply.github.com>
2024-05-11 22:06:24 +0200

    Working on articles page

--- app/src/Components/ArticleCard/index.js
+++ app/src/Components/ArticleCard/index.js
@@ -6,22 +6,24 @@ import CardMedia from "@mui/material/CardMedia";
 import Button from "@mui/material/Button";
 import Typography from "@mui/material/Typography";
 
-export default function MediaCard() {
+export default function ArticleCard({ title, previewImage, mediumURL, previewText }) {
+  console.log(mediumURL);
+
   return (
     <Card sx={{ maxWidth: 345 }}>
-      <CardMedia sx={{ height: 140 }} image="/static/images/cards/contemplative-reptile.jpg" title="green iguana" />
+      <CardMedia sx={{ height: 140 }} image={previewImage} title="green iguana" />
       <CardContent>
         <Typography gutterBottom variant="h5" component="div">
-          Lizard
+          {title}
         </Typography>
         <Typography variant="body2" color="text.secondary">
-          Lizards are a widespread group of squamate reptiles, with over 6,000 species, ranging across all continents
-          except Antarctica
+          {previewText}
         </Typography>
       </CardContent>
       <CardActions>
-        <Button size="small">Share</Button>
-        <Button size="small">Learn More</Button>
+        <Button href={mediumURL} size="small">
+          Read More
+        </Button>
       </CardActions>
     </Card>
   );
--- app/src/Components/Articles/index.js
+++ app/src/Components/Articles/index.js
@@ -2,6 +2,8 @@ import React from "react";
 import "./style.css";
 import ArticleCard from "../ArticleCard";
 
+const articlesJson = require("../../Data/articles.json");
+
 class ArticlePage extends React.Component {
   constructor(props) {
     super(props);
@@ -19,7 +21,34 @@ class ArticlePage extends React.Component {
   };
 
   render() {
-    return <ArticleCard></ArticleCard>;
+    return (
+      <div className="TechPageContainer">
+        <section className="landing-section my-projects">
+          <h2>Selected Articles</h2>
+          <hr />
+
+          <div className="project-container content-padding">
+            {articlesJson.articles.map((article) => {
+              return (
+                <ArticleCard
+                  id={article.id}
+                  title={article.title}
+                  previewImage={article.previewImage}
+                  previewText={article.previewText}
+                  mediumURL={article.mediumUrl}
+                />
+              );
+            })}
+          </div>
+
+          <div className="button-container-tech">
+            <a href="https://medium.com/@becki.wordsworth" target="_blank" className="button-tech">
+              Visit Medium
+            </a>
+          </div>
+        </section>
+      </div>
+    );
   }
 }
 
