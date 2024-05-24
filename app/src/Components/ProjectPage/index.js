4de32f5470344812b66db5652ccd7dfc1213eea6
Becki Wordsworth <63713673+BeckiWordsworth@users.noreply.github.com>
2024-05-24 23:14:43 +0200

    Fixing issues with project and rebuilding

--- app/src/Components/ProjectPage/index.js
+++ app/src/Components/ProjectPage/index.js
@@ -1,11 +1,11 @@
 import React, { useState, useEffect } from "react";
-import { Link, useParams } from "react-router-dom";
+import { useParams } from "react-router-dom";
 
 import "./style.css";
 
 const projectsJson = require("../../Data/projects.json");
 
-const ProductPage = ({}) => {
+const ProductPage = () => {
   const [project, setProject] = useState("");
   const { id } = useParams();
 
@@ -16,125 +16,63 @@ const ProductPage = ({}) => {
   useEffect(() => {
     console.log(projectsJson);
 
-    const project = projectsJson.projects.find((proj) => {
-      if (proj.id == id) {
-        return true;
-      } else {
-        return false;
-      }
+    const newProject = projectsJson.projects.find((proj) => {
+      return proj.id == id;
     });
 
-    setProject(JSON.stringify(project));
+    console.log({ newProject });
+    setProject(newProject);
   }, []);
 
-  console.log(project);
+  console.log({ project });
+
+  if (!project) {
+    return <div>Loading...</div>;
+  }
+
+  let demoLink = "";
+  let gitLink = "";
+
+  if (project) {
+    if (project.demoUrl) {
+      demoLink = (
+        <a href={project.demoUrl} className="button">
+          See it Live
+        </a>
+      );
+    }
+
+    if (project.gitUrl) {
+      gitLink = (
+        <a href={project.gitUrl} className="button">
+          Github Repo
+        </a>
+      );
+    }
+  }
 
   return (
-    <div>Hola</div>
-    // <div className="project-page">
-    //   <div className="project-page-writing content-padding">
-    //     <h1>{project.title}</h1>
-    //     <img className="project-image" src={"/" + project.heroImage} alt="project Image" />
-    //     <p>{project.description}</p>
-    //   </div>
+    <div className="project-page">
+      <div className="project-page-writing content-padding">
+        <h1>{project.title}</h1>
+        <img className="project-image" src={"/" + project.heroImage} alt="project Image" />
+        <p>{project.description}</p>
+      </div>
 
-    //   <div className="button-container content-padding">{{demoLink}
-    //     {gitLink}
-    // </div>
+      <div className="button-container content-padding">
+        {demoLink}
+        {gitLink}
+      </div>
 
-    //   <section className="project-page-section content-padding">
-    //     <h2>Specs</h2>
-    //     <hr />
-
-    //     <div className="tech-specs-container">
-    //       <div className="toolbox-component">
-    //         <h3>Tech</h3>
-    //         <ul>
-    //           {this.state.project.techSpecs.map((item) => { */}
-    //             return <li>{item}</li>;
-    //           })}
-    //         </ul>
-    //       </div>
-
-    //       <div className="toolbox-component">
-    //         <h3>Toolbox</h3>
-    //         <ul>
-    //           {this.state.project.toolBox.map((item) => {
-    //             return <li>{item}</li>;
-    //           })}
-    //         </ul>
-    //       </div>
-
-    //       <div className="toolbox-component">
-    //         <h3>More</h3>
-    //         <ul>
-    //           {this.state.project.more.map((item) => {
-    //             return <li>{item}</li>;
-    //           })}
-    //         </ul>
-    //       </div>
-    //     </div>
-    //   </section>
-    // </div>
+      <section className="project-page-section content-padding">
+        <h2>Specs</h2>
+        <hr />
+      </section>
+    </div>
   );
 };
 
 export default ProductPage;
-
-// const ProductPage = ({}) => {
-
-// const [project, setProject] = useState("");
-// const { id } = useParams();
-
-//   const product = products.find((p) => String(p._id) === id);
-
-// useEffect(() => {
-
-//   const project = projectsJson.projects.find((project) => {
-//     if (project.id == id) {
-//       return true;
-//     } else {
-//       return false;
-//     }
-//   });
-
-//   setProject(project)
-//   }, []);
-
-// componentDidMount() {
-//   const projectId = this.props.match.params.id;
-//   const project = projectsJson.projects.find((project) => {
-//     if (project.id == projectId) {
-//       return true;
-//     } else {
-//       return false;
-//     }
-//   });
-
-//   this.setState({
-//     project: project,
-//   });
-// }
-
-//   if (project) {
-//     let demoLink = "";
-//     let gitLink = "";
-
-//     if (project.demoUrl) {
-//       demoLink = (
-//         <a href={project.demoUrl} className="button">
-//           See it Live
-//         </a>
-//       );
-//     }
-
-//     if (project.gitUrl) {
-//       gitLink = (
-//         <a href={project.gitUrl} className="button">
-//           Github Repo
-//         </a>
-//       );
-//     }
 
 //       return (
 // <div className="project-page">
@@ -165,11 +103,13 @@ export default ProductPage;
 
 //       <div className="toolbox-component">
 //         <h3>Toolbox</h3>
-//         <ul>
-//           {this.state.project.toolBox.map((item) => {
-//             return <li>{item}</li>;
-//           })}
-//         </ul>
+{
+  /* <ul>
+  {this.state.project.toolBox.map((item) => {
+    return <li>{item}</li>;
+  })}
+</ul>; */
+}
 //       </div>
 
 //       <div className="toolbox-component">
@@ -178,12 +118,12 @@ export default ProductPage;
 //           {this.state.project.more.map((item) => {
 //             return <li>{item}</li>;
 //           })}
-//         </ul>
+// //         </ul>
 //       </div>
 //     </div>
 //   </section>
 // </div>
-//       );
+// //       );
 //     } else {
 //       return <div>NO PROJECT FOUND</div>;
 //     }
