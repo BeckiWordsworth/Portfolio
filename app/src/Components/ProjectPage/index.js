fbdb67c36d4ffb854509aeeb7532b2687f58c02d
Becki Wordsworth <63713673+BeckiWordsworth@users.noreply.github.com>
2024-05-24 22:43:55 +0200

    Rebuild of productpage

--- app/src/Components/ProjectPage/index.js
+++ app/src/Components/ProjectPage/index.js
@@ -1,123 +1,193 @@
 import React, { useState, useEffect } from "react";
-import { Link, useParams } from 'react-router-dom';
+import { Link, useParams } from "react-router-dom";
 
 import "./style.css";
 
 const projectsJson = require("../../Data/projects.json");
 
-
 const ProductPage = ({}) => {
-
-  const [project, setProduct] = useState("");
+  const [project, setProject] = useState("");
   const { id } = useParams();
 
-  const product = products.find((p) => String(p._id) === id);
+  console.log(projectsJson);
 
-// class ProductPage extends React.Component {
-//   constructor(props) {
-//     super(props);
+  console.log(id);
 
-//     this.state = {};
-//   }
+  useEffect(() => {
+    console.log(projectsJson);
 
-//   let pr
-
-useEffect(() => {
-  const connection = createConnection(serverUrl, roomId);
-  connection.connect();
-  return () => {
-    connection.disconnect();
-  };
-}, [serverUrl, roomId]);
-
-  componentDidMount() {
-    const projectId = this.props.match.params.id;
-    const project = projectsJson.projects.find((project) => {
-      if (project.id == projectId) {
+    const project = projectsJson.projects.find((proj) => {
+      if (proj.id == id) {
         return true;
       } else {
         return false;
       }
     });
 
-    this.setState({
-      project: project,
-    });
-  }
+    setProject(JSON.stringify(project));
+  }, []);
 
-  render() {
-    if (this.state.project) {
-      let demoLink = "";
-      let gitLink = "";
+  console.log(project);
 
-      if (this.state.project.demoUrl) {
-        demoLink = (
-          <a href={this.state.project.demoUrl} className="button">
-            See it Live
-          </a>
-        );
-      }
+  return (
+    <div>Hola</div>
+    // <div className="project-page">
+    //   <div className="project-page-writing content-padding">
+    //     <h1>{project.title}</h1>
+    //     <img className="project-image" src={"/" + project.heroImage} alt="project Image" />
+    //     <p>{project.description}</p>
+    //   </div>
 
-      if (this.state.project.gitUrl) {
-        gitLink = (
-          <a href={this.state.project.gitUrl} className="button">
-            Github Repo
-          </a>
-        );
-      }
+    //   <div className="button-container content-padding">{{demoLink}
+    //     {gitLink}
+    // </div>
 
-      return (
-        <div className="project-page">
-          <div className="project-page-writing content-padding">
-            <h1>{this.state.project.title}</h1>
-            <img className="project-image" src={"/" + this.state.project.heroImage} alt="project Image" />
-            <p>{this.state.project.description}</p>
-          </div>
+    //   <section className="project-page-section content-padding">
+    //     <h2>Specs</h2>
+    //     <hr />
 
-          <div className="button-container content-padding">
-            {demoLink}
-            {gitLink}
-          </div>
+    //     <div className="tech-specs-container">
+    //       <div className="toolbox-component">
+    //         <h3>Tech</h3>
+    //         <ul>
+    //           {this.state.project.techSpecs.map((item) => { */}
+    //             return <li>{item}</li>;
+    //           })}
+    //         </ul>
+    //       </div>
 
-          <section className="project-page-section content-padding">
-            <h2>Specs</h2>
-            <hr />
+    //       <div className="toolbox-component">
+    //         <h3>Toolbox</h3>
+    //         <ul>
+    //           {this.state.project.toolBox.map((item) => {
+    //             return <li>{item}</li>;
+    //           })}
+    //         </ul>
+    //       </div>
 
-            <div className="tech-specs-container">
-              <div className="toolbox-component">
-                <h3>Tech</h3>
-                <ul>
-                  {this.state.project.techSpecs.map((item) => {
-                    return <li>{item}</li>;
-                  })}
-                </ul>
-              </div>
-
-              <div className="toolbox-component">
-                <h3>Toolbox</h3>
-                <ul>
-                  {this.state.project.toolBox.map((item) => {
-                    return <li>{item}</li>;
-                  })}
-                </ul>
-              </div>
-
-              <div className="toolbox-component">
-                <h3>More</h3>
-                <ul>
-                  {this.state.project.more.map((item) => {
-                    return <li>{item}</li>;
-                  })}
-                </ul>
-              </div>
-            </div>
-          </section>
-        </div>
-      );
-    } else {
-      return <div>NO PROJECT FOUND</div>;
-    }
-  }
-}
+    //       <div className="toolbox-component">
+    //         <h3>More</h3>
+    //         <ul>
+    //           {this.state.project.more.map((item) => {
+    //             return <li>{item}</li>;
+    //           })}
+    //         </ul>
+    //       </div>
+    //     </div>
+    //   </section>
+    // </div>
+  );
+};
 
 export default ProductPage;
+
+// const ProductPage = ({}) => {
+
+// const [project, setProject] = useState("");
+// const { id } = useParams();
+
+//   const product = products.find((p) => String(p._id) === id);
+
+// useEffect(() => {
+
+//   const project = projectsJson.projects.find((project) => {
+//     if (project.id == id) {
+//       return true;
+//     } else {
+//       return false;
+//     }
+//   });
+
+//   setProject(project)
+//   }, []);
+
+// componentDidMount() {
+//   const projectId = this.props.match.params.id;
+//   const project = projectsJson.projects.find((project) => {
+//     if (project.id == projectId) {
+//       return true;
+//     } else {
+//       return false;
+//     }
+//   });
+
+//   this.setState({
+//     project: project,
+//   });
+// }
+
+//   if (project) {
+//     let demoLink = "";
+//     let gitLink = "";
+
+//     if (project.demoUrl) {
+//       demoLink = (
+//         <a href={project.demoUrl} className="button">
+//           See it Live
+//         </a>
+//       );
+//     }
+
+//     if (project.gitUrl) {
+//       gitLink = (
+//         <a href={project.gitUrl} className="button">
+//           Github Repo
+//         </a>
+//       );
+//     }
+
+//       return (
+// <div className="project-page">
+//   <div className="project-page-writing content-padding">
+//     <h1>{this.state.project.title}</h1>
+//     <img className="project-image" src={"/" + this.state.project.heroImage} alt="project Image" />
+//     <p>{this.state.project.description}</p>
+//   </div>
+
+//   <div className="button-container content-padding">
+//     {demoLink}
+//     {gitLink}
+//   </div>
+
+//   <section className="project-page-section content-padding">
+//     <h2>Specs</h2>
+//     <hr />
+
+//     <div className="tech-specs-container">
+//       <div className="toolbox-component">
+//         <h3>Tech</h3>
+//         <ul>
+//           {this.state.project.techSpecs.map((item) => {
+//             return <li>{item}</li>;
+//           })}
+//         </ul>
+//       </div>
+
+//       <div className="toolbox-component">
+//         <h3>Toolbox</h3>
+//         <ul>
+//           {this.state.project.toolBox.map((item) => {
+//             return <li>{item}</li>;
+//           })}
+//         </ul>
+//       </div>
+
+//       <div className="toolbox-component">
+//         <h3>More</h3>
+//         <ul>
+//           {this.state.project.more.map((item) => {
+//             return <li>{item}</li>;
+//           })}
+//         </ul>
+//       </div>
+//     </div>
+//   </section>
+// </div>
+//       );
+//     } else {
+//       return <div>NO PROJECT FOUND</div>;
+//     }
+//     );
+//   };
+
+// export default ProductPage;
