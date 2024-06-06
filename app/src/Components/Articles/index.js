6cb857747c44796afa1fed94c73e9ae0c7dd45bb
Becki Wordsworth <63713673+BeckiWordsworth@users.noreply.github.com>
2024-06-06 18:08:17 +0200

    Making articles page a functional component

--- app/src/Components/Articles/index.js
+++ app/src/Components/Articles/index.js
@@ -1,56 +1,38 @@
 import React from "react";
 import "./style.css";
-import Grid from "@mui/material/Grid";
 import ArticleCard from "../ArticleCard";
 
 const articlesJson = require("../../Data/articles.json");
 
-class ArticlePage extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      name: "",
-      email: "",
-      message: "",
-    };
-  }
+const ArticlePage = () => {
+  return (
+    <div className="TechPageContainer">
+      <section className="landing-section my-projects">
+        <h2>Selected Articles</h2>
+        <hr />
 
-  handleSubmit = () => {};
+        <div className="project-container content-padding">
+          {articlesJson.articles.map((article) => {
+            return (
+              <ArticleCard
+                id={article.id}
+                title={article.title}
+                previewImage={article.previewImage}
+                previewText={article.previewText}
+                mediumURL={article.mediumUrl}
+              />
+            );
+          })}
+        </div>
 
-  queryChange = (evt) => {
-    this.setState({ query: evt.target.value });
-  };
-
-  render() {
-    return (
-      <div className="TechPageContainer">
-        <section className="landing-section my-projects">
-          <h2>Selected Articles</h2>
-          <hr />
-
-          <div className="project-container content-padding">
-            {articlesJson.articles.map((article) => {
-              return (
-                <ArticleCard
-                  id={article.id}
-                  title={article.title}
-                  previewImage={article.previewImage}
-                  previewText={article.previewText}
-                  mediumURL={article.mediumUrl}
-                />
-              );
-            })}
-          </div>
-
-          <div className="button-container-tech">
-            <a href="https://medium.com/@becki.wordsworth" target="_blank" className="button-tech">
-              Visit Medium
-            </a>
-          </div>
-        </section>
-      </div>
-    );
-  }
-}
+        <div className="button-container-tech">
+          <a href="https://medium.com/@becki.wordsworth" target="_blank" className="button-tech">
+            Visit Medium
+          </a>
+        </div>
+      </section>
+    </div>
+  );
+};
 
 export default ArticlePage;
