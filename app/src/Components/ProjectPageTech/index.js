5ca548e1c0dfd7abc26f33664305977c28bfab8e
Becki Wordsworth <63713673+BeckiWordsworth@users.noreply.github.com>
2024-06-07 20:14:13 +0200

    Updating project previews and more

--- app/src/Components/ProjectPageTech/index.js
+++ app/src/Components/ProjectPageTech/index.js
@@ -6,44 +6,39 @@ import "./style.css";
 
 const projectsJson = require("../../Data/projects.json");
 
-class ProjectPageTech extends React.Component {
-  render() {
-    return (
-      <div className="TechPageContainer">
-        <section className="landing-section my-projects">
-          <h2>Selected Projects</h2>
-          <hr />
+const ProjectPageTech = () => {
+  return (
+    <div className="TechPageContainer">
+      <section className="landing-section my-projects">
+        <h2>Selected Projects</h2>
+        <hr />
 
-          <div className="project-container content-padding">
-            {projectsJson.projects.map(project => {
-              return (
-                <ProjectPreview
-                  id={project.id}
-                  name={project.title}
-                  icon={project.icon}
-                  image={project.previewImage}
-                  shortDescription={project.shortDescription}
-                  veryShortDescription={project.veryShortDescription}
-                  Description={project.Description}
-                  deliveryTime={project.deliveryTime}
-                  gitURL={project.gitURL}
-                />
-              );
-            })}
-          </div>
+        <div className="project-container content-padding">
+          {projectsJson.projects.map((project) => {
+            return (
+              <ProjectPreview
+                id={project.id}
+                name={project.title}
+                icon={project.icon}
+                image={project.previewImage}
+                shortDescription={project.shortDescription}
+                veryShortDescription={project.veryShortDescription}
+                Description={project.Description}
+                deliveryTime={project.deliveryTime}
+                gitURL={project.gitURL}
+              />
+            );
+          })}
+        </div>
 
-          <div className="button-container-tech">
-            <a
-              href="https://github.com/BeckiWordsworth"
-              className="button-tech"
-            >
-              Visit Github
-            </a>
-          </div>
-        </section>
-      </div>
-    );
-  }
-}
+        <div className="button-container-tech">
+          <a href="https://github.com/BeckiWordsworth" className="button-tech">
+            Visit Github
+          </a>
+        </div>
+      </section>
+    </div>
+  );
+};
 
 export default ProjectPageTech;
--- app/src/Components/ProjectPreview/index.js
+++ app/src/Components/ProjectPreview/index.js
@@ -1,23 +1,23 @@
-import React from "react"
-import "./style.css"
+import React from "react";
+import "./style.css";
 
-class ProjectPreview extends React.Component {
-
-  render() {
-    return (
-      <div className="project-preview" onClick={() => {window.location.href = "/project/" + this.props.id}}>
-        <div className="project-preview-inner">
-          <div className="project-preview-top">
-            <img src={"/" + this.props.icon} />
-            <p>{this.props.name}</p>
-          </div>
-          <div className="project-preview-bottom">
-            {this.props.veryShortDescription}
-          </div>
+const ProjectPreview = () => {
+  return (
+    <div
+      className="project-preview"
+      onClick={() => {
+        window.location.href = "/project/" + this.props.id;
+      }}
+    >
+      <div className="project-preview-inner">
+        <div className="project-preview-top">
+          <img src={"/" + this.props.icon} />
+          <p>{this.props.name}</p>
         </div>
+        <div className="project-preview-bottom">{this.props.veryShortDescription}</div>
       </div>
-    )
-  }
-}
+    </div>
+  );
+};
 
-export default ProjectPreview
+export default ProjectPreview;
