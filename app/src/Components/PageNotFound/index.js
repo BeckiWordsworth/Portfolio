903acb5f9f076b45a39d8281ec2f69c60ab1ab7b
Becki Wordsworth <63713673+BeckiWordsworth@users.noreply.github.com>
2024-06-08 20:47:23 +0200

    Page not found

--- app/src/Components/PageNotFound/index.js
+++ app/src/Components/PageNotFound/index.js
@@ -1,17 +1,15 @@
 import React from "react";
-import './style.css'
+import "./style.css";
 
-class PageNotFound extends React.Component {
-  render() {
-    return (
-      <div className="hero-about">
-          <img src="/javascript.jpg" />
-          <div className="hero-text-about">
-            <h1>Page Not Found</h1>
-          </div>
+const PageNotFound = () => {
+  return (
+    <div className="hero-about">
+      <img src="/javascript.jpg" />
+      <div className="hero-text-about">
+        <h1>Page Not Found</h1>
       </div>
-    )
-  }
-}
+    </div>
+  );
+};
 
-export default PageNotFound
+export default PageNotFound;
