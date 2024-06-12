dbdd40a9a8b1a704b678d21a0bbd85150feb4719
Becki Wordsworth <63713673+BeckiWordsworth@users.noreply.github.com>
2024-06-12 21:38:29 +0200

    Missing alt tags

--- app/src/Components/PageNotFound/index.js
+++ app/src/Components/PageNotFound/index.js
@@ -4,7 +4,7 @@ import "./style.css";
 const PageNotFound = () => {
   return (
     <div className="hero-about">
-      <img src="/javascript.jpg" />
+      <img src="/javascript.jpg" alt="javascript logo" />
       <div className="hero-text-about">
         <h1>Page Not Found</h1>
       </div>
